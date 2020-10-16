c1d21835ab519fcb732a95317ef3af4364eb818b
baxa2910 <bohodir29102001@gmail.com>
2020-10-16 05:55:03 +0500

    update about me

--- client/src/components/main/AboutMe.js
+++ client/src/components/main/AboutMe.js
@@ -30,10 +30,7 @@ function AboutMe() {
       </div>
       <div className="information">
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.{' '}
+          Men Rahmonov Bohodir ishlshni yaxshi koraman oz ustimda{' '}
         </p>
       </div>
       <div className="skill-container">
